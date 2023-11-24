ab1c669d75b1f54da9ff141ce1f02e3d6027be92
ManikDidige <151952645+ManikDidige@users.noreply.github.com>
2023-11-24 22:33:31 +0530

    Add files via upload

---
+++ quadratic.java
@@ -0,0 +1,32 @@
+public class quadratic {
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java QuadraticEquationSolverCommandLine <a> <b> <c>");
+            System.exit(1);
+        }
+
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Root: " + root);
+        } else {
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+
+            System.out.println("Root 1: " + realPart + " + " + imaginaryPart + "i");
+            System.out.println("Root 2: " + realPart + " - " + imaginaryPart + "i");
+        }
+    }
+}
